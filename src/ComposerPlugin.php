d74086e828b5510f8545957d0ca6122b651b265e
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2025-04-06 17:39:27 +0200

    Implement Rector upgrade process in ComposerPlugin

--- src/ComposerPlugin.php
+++ src/ComposerPlugin.php
@@ -42,30 +42,53 @@ class ComposerPlugin implements PluginInterface, EventSubscriberInterface
     public function onPostUpdate(Event $event): void
     {
         $io = $event->getIO();
+        $composer = $event->getComposer();
 
         if (!$this->isDevelopmentEnvironment()) {
             return;
         }
 
-//        if (!class_exists('Rector\Config\RectorConfig\RectorConfig')) {
-//            $io->write('<info>To do so, run: composer require rector/rector</info>');
-//            return;
-//        }
+        if (!class_exists('Rector\Config\RectorConfig') && !file_exists('vendor/bin/rector')) {
+            $io->write('<warning>Rector is not installed. No automatic updates will be performed.</warning>');
+            return;
+        }
 
-        var_dump([
-            'composer' => [
-                'name' => $event->getComposer()->getPackage()->getName(),
-                'version' => $event->getComposer()->getPackage()->getVersion(),
-                'fullPrettyVersion' => $event->getComposer()->getPackage()->getFullPrettyVersion(),
-            ],
-        ]);
-        $event->getComposer()->getPackage()->getVersion();
+        $localRepo = $composer->getRepositoryManager()->getLocalRepository();
+        $installationManager = $composer->getInstallationManager();
+        $packages = $localRepo->getPackages();
+
+        $upgradesToRun = [];
+
+        foreach ($packages as $package) {
+            $packageName = $package->getName();
+            $version = $package->getFullPrettyVersion();
+            $installPath = $installationManager->getInstallPath($package);
+
+            $rectorConfigPath = $installPath . '/config/rector/upgrade/' . $version . '.php';
+
+            if (file_exists($rectorConfigPath)) {
+                $upgradesToRun[] = [
+                    'package' => $packageName,
+                    'version' => $version,
+                    'config_path' => $rectorConfigPath,
+                ];
+            }
+        }
+
+        if (empty($upgradesToRun)) {
+            $io->write('<info>No Rector upgrade will be performed.</info>');
+            return;
+        }
+
+        foreach ($upgradesToRun as $upgrade) {
+            $this->runRectorUpgrade($upgrade, $io);
+        }
     }
 
     private function isDevelopmentEnvironment(): bool
     {
-        $composerJson = json_decode(file_get_contents(getcwd() . '/composer.json'), true);
-        if (isset($composerJson['require-dev']['rector/rector'])) {
+
+        if (file_exists('vendor/bin/rector')) {
             return true;
         }
 
@@ -73,11 +96,64 @@ class ComposerPlugin implements PluginInterface, EventSubscriberInterface
             return true;
         }
 
-        global $argv;
-        if (in_array('--dev', $argv, true)) {
+        if (getenv('APP_ENV') !== 'prod' && getenv('APP_ENV') !== 'production') {
             return true;
         }
 
         return false;
+    }
+
+    private function runRectorUpgrade(array $upgrade, IOInterface $io): void
+    {
+        $packageName = $upgrade['package'];
+        $version = $upgrade['version'];
+        $configPath = $upgrade['config_path'];
+
+        $io->write(sprintf(
+            '<info>Running Rector upgrade for %s (version %s)</info>',
+            $packageName,
+            $version,
+        ));
+
+        $tempConfigFile = $this->createTemporaryConfig($configPath);
+
+        $command = sprintf('vendor/bin/rector process --config %s', $tempConfigFile);
+        $io->write(sprintf('<info>Command: %s</info>', $command));
+
+        exec($command, $output, $returnCode);
+
+        @unlink($tempConfigFile);
+
+        if ($returnCode !== 0) {
+            $io->write('<error>Upgrade failed for ' . $packageName . '</error>');
+            $io->write(implode("\n", $output));
+        } else {
+            $io->write('<info>Upgrade successful for ' . $packageName . '</info>');
+        }
+    }
+
+    private function createTemporaryConfig(string $packageConfigPath): string
+    {
+        $tempFile = sys_get_temp_dir() . '/rector_upgrade_' . uniqid() . '.php';
+
+        $config = <<<PHP
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig \$rectorConfig): void {
+    \$rectorConfig->paths([
+        getcwd() . '/src',
+    ]);
+    
+    require_once '{$packageConfigPath}';
+};
+PHP;
+
+        file_put_contents($tempFile, $config);
+
+        return $tempFile;
     }
 }
